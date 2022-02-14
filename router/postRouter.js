6860b08fd6eaa0afe03f36e9a6b57b0ae9d87755
ayrAtMaterGog63 <airatgafurow@yandex.ru>
2022-02-14 22:43:19 +0300

    dd

--- router/postRouter.js
+++ router/postRouter.js
@@ -4,6 +4,7 @@ const auth = require("../middlewares/auth");
 const router = new Router()
 
 
+router.get('/', postController.getAll)
 router.post('/', auth, postController.create)
 router.delete('/:id', auth, postController.delete)
 router.patch('/:id', auth, postController.edit)
