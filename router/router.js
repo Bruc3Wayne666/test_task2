b66eff407887b7a23abdf85c806e4bdd01e3efb2
ayrAtMaterGog63 <airatgafurow@yandex.ru>
2022-02-14 18:05:47 +0300

    Initial

--- router/router.js
+++ router/router.js
@@ -4,6 +4,9 @@ const authRouter = require('../router/authRouter')
 const postRouter = require('../router/postRouter')
 
 
+router.use('/', (req, res) => {
+    return '<h1>Hello</h1>'
+})
 router.use('/auth', authRouter)
 router.use('/post', postRouter)
 
